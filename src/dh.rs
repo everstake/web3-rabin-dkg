d3a90d514b2a2f6b4a1616153e758b30c410ffad
Vadim Grozinok <vadim.g@blaize.tech>
2020-11-10 09:17:02 +0000

    Write tests for Diffie Hellman protocol

--- src/dh.rs
+++ src/dh.rs
@@ -51,3 +51,60 @@ pub fn context(dealer: &GE, verifiers: &Vec<GE>) -> Vec<u8> {
     }
     contx
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::curve_traits;
+    use crate::ristretto_curve;
+    use aead::{generic_array::GenericArray, Aead, Payload};
+    use curve_traits::{ECPoint, ECScalar};
+    use ristretto_curve::{FE, GE};
+
+    fn make_context() -> Vec<u8> {
+        let generator: GE = ECPoint::generator();
+
+        let priv1: FE = ECScalar::from(168 as u64);
+        let priv2: FE = ECScalar::from(54 as u64);
+        let priv3: FE = ECScalar::from(2089 as u64);
+        let priv4: FE = ECScalar::from(8962 as u64);
+        let priv5: FE = ECScalar::from(362 as u64);
+
+        let pub_1: GE = generator.scalar_mul(&priv1.get_element());
+        let pub_2: GE = generator.scalar_mul(&priv2.get_element());
+        let pub_3: GE = generator.scalar_mul(&priv3.get_element());
+        let pub_4: GE = generator.scalar_mul(&priv4.get_element());
+        let pub_dealer: GE = generator.scalar_mul(&priv5.get_element());
+
+        let verifiers: Vec<GE> = vec![pub_1, pub_2, pub_3, pub_4];
+
+        super::context(&pub_dealer, &verifiers)
+    }
+
+    #[test]
+    fn test_aead() {
+        let generator: GE = ECPoint::generator();
+        let priv1: FE = ECScalar::from(19846 as u64);
+        let pub_1: GE = generator.scalar_mul(&priv1.get_element());
+
+        let context = make_context();
+        let aead = super::new_aead(&pub_1, &context);
+
+        let pay = Payload {
+            msg: b"super phrase".as_ref(),
+            aad: context.as_ref(),
+        };
+        let nonce = GenericArray::from_slice(&[0u8; 12]);
+
+        let ciphertext = aead.encrypt(nonce, pay).expect("encryption failure!");
+        let plaintext = aead
+            .decrypt(
+                nonce,
+                Payload {
+                    msg: ciphertext.as_ref(),
+                    aad: context.as_ref(),
+                },
+            )
+            .expect("decryption failure!");
+        assert_eq!(&plaintext, b"super phrase");
+    }
+}
