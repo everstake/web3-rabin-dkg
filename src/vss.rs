d5d37c8b715a84c9d93709e4642548e6f1e5d4f0
newcodepusher <svyatoslav.d@blaize.tech>
2020-12-09 12:02:52 +0000

    Implement Verifier struct

--- src/vss.rs
+++ src/vss.rs
@@ -151,6 +151,273 @@ pub struct Verifier {
     aggregator: Aggregator,
 }
 
+impl Verifier {
+    /// new_verifier returns a Verifier out of:
+    /// - its longterm secret key
+    /// - the longterm dealer public key
+    /// - the list of public key of verifiers. The list MUST include the public key
+    /// of this Verifier also.
+    /// The security parameter t of the secret sharing scheme is automatically set to
+    /// a default safe value. If a different t value is required, it is possible to set
+    /// it with verifier.set_t().
+    pub fn new(longterm: FE, dealer: GE, verifiers: Vec<GE>) -> Result<Verifier, Box<dyn Error>> {
+        let verifiers: Rc<[GE]> = verifiers.into();
+
+        let generator = GE::generator();
+        let pub_k: GE = generator.scalar_mul(&longterm.get_element());
+
+        let index = verifiers
+            .iter()
+            .position(|point| point == &pub_k)
+            .map(|i| i as u32)
+            .ok_or_else(|| simple_error!("vss: public key not found in the list of verifiers"))?;
+        let hkdf_context = dh::context(&dealer, &verifiers);
+
+        let aggregator = Aggregator::new(dealer, verifiers.clone(), 0, Vec::new());
+
+        Ok(Verifier {
+            longterm,
+            pub_k,
+            dealer,
+            verifiers,
+            hkdf_context,
+            index,
+            aggregator,
+        })
+    }
+
+    // process_encrypted_deal decrypt the deal received from the Dealer.
+    /// If the deal is valid, i.e. the verifier can verify its shares
+    /// against the public coefficients and the signature is valid, an approval
+    /// response is returned and must be broadcasted to every participants
+    /// including the dealer.
+    /// If the deal itself is invalid, it returns a complaint response that must be
+    /// broadcasted to every other participants including the dealer.
+    /// If the deal has already been received, or the signature generation of the
+    /// response failed, it returns an error without any responses.
+    pub fn process_encrypted_deal(
+        &mut self,
+        encr_d: &EncryptedDeal,
+    ) -> Result<Response, Box<dyn Error>> {
+        let deal = self.decrypt_deal(encr_d)?;
+
+        if deal.sec_share.i != self.index {
+            bail!("vss: verifier got wrong index from deal")
+        }
+
+        let session_id = session_id(&self.dealer, self.verifiers(), &deal.commitments, deal.t);
+
+        if deal.session_id != session_id {
+            bail!("vss: session id doesn't match");
+        }
+
+        if self.aggregator.threshold == 0 {
+            self.aggregator = Aggregator::new(
+                self.dealer,
+                self.verifiers.clone(),
+                deal.t,
+                deal.session_id.clone(),
+            );
+        }
+
+        if self.aggregator.deal.t != 0 {
+            bail!("vss: verifier already received a deal")
+        }
+
+        self.aggregator.session_id = deal.session_id.clone();
+        self.aggregator.deal = deal.clone();
+
+        let approved = deal.verify(&self.aggregator.verifiers, &session_id).is_ok();
+
+        let r_hash = Response::hash(&session_id, self.index, approved as u32)?;
+        let signature = sign::sign_msg(
+            self.longterm.get_element().to_bytes(),
+            self.pub_k.get_element().to_bytes(),
+            &r_hash,
+            &self.index.to_le_bytes(),
+        )?;
+
+        let response = Response {
+            index: self.index,
+            session_id: session_id.to_vec(),
+            approved,
+            signature,
+        };
+
+        self.aggregator.add_response(&response)?;
+
+        Ok(response)
+    }
+
+    pub fn decrypt_deal(&mut self, encr_d: &EncryptedDeal) -> Result<Deal, Box<dyn Error>> {
+        let eph_buff = encr_d.dh_key.get_element().to_bytes();
+
+        // verify signature
+        sign::verify_signature(
+            self.dealer.get_element().to_bytes().as_ref(),
+            encr_d.signature.as_ref(),
+            eph_buff.as_ref(),
+            self.index.to_le_bytes().as_ref(),
+        )
+        .map_err(|e| simple_error!("vss: signature verification failed: {}", e))?;
+
+        // compute shared key and AES526-GCM cipher
+        let pre: GE = dh::dh_exchange(&self.longterm, &encr_d.dh_key);
+        let gcm = dh::new_aead(&pre, &self.hkdf_context);
+        let nonce = GenericArray::from_slice(encr_d.nonce.as_slice());
+        let decrypted = gcm.decrypt(
+            nonce,
+            Payload {
+                msg: encr_d.cipher.as_ref(),
+                aad: self.hkdf_context.as_ref(),
+            },
+        );
+        let decrypted_vec =
+            decrypted.map_err(|_| simple_error!("vss: failed decrypt AES526-GCM cipher deal"))?;
+        let decoded: Deal = bincode::deserialize(&decrypted_vec[..])?;
+
+        Ok(decoded)
+    }
+
+    pub fn deal_certified(&self) -> bool {
+        self.aggregator.deal_certified()
+    }
+
+    pub fn enough_approvals(&self) -> bool {
+        self.aggregator.enough_approvals()
+    }
+
+    // process_justification takes a Justification from dealer and returns an error if
+    // something went wrong during the verification. If it is the case, that
+    // probably means the Dealer is acting maliciously. In order to be sure, call
+    // `v.EnoughApprovals()` and if true, `v.DealCertified()`.
+    pub fn process_justification(
+        &mut self,
+        justification: &Justification,
+    ) -> Result<(), Box<dyn Error>> {
+        if self.aggregator.verifiers.len() <= justification.index as usize {
+            bail!("vss: index out of bounds in justification")
+        }
+
+        // copy our session id to use later when we borrow mut self
+        let session_id = self.session_id().to_vec();
+        if session_id != justification.deal.session_id {
+            bail!("vss: session id doesn't match");
+        }
+
+        let resp: &mut Response = self
+            .aggregator
+            .responses
+            .get_mut(&justification.index)
+            .ok_or_else(|| simple_error!("vss: no complaints received for this justification"))?;
+
+        if resp.approved {
+            bail!("vss: justification received for an approval")
+        }
+
+        // if aggregator isn't fully initialized
+        if self.aggregator.deal.t == 0 {
+            self.aggregator.session_id = justification.deal.session_id.clone();
+            self.aggregator.deal = justification.deal.clone();
+        }
+
+        let verif = justification
+            .deal
+            .verify(&self.aggregator.verifiers, &session_id);
+
+        if let Err(e) = verif {
+            self.aggregator.bad_dealer = true;
+            bail!(e);
+        }
+
+        resp.approved = true;
+
+        Ok(())
+    }
+
+    pub fn set_timeout(&mut self) {
+        self.aggregator.clean_verifiers();
+    }
+
+    // index returns the index of the verifier in the list of participants used
+    // during this run of the protocol.
+    pub fn index(&self) -> u32 {
+        self.index
+    }
+
+    // key returns the longterm key pair this verifier is using during this protocol
+    // run.
+    pub fn key(&self) -> (FE, GE) {
+        (self.longterm, self.pub_k)
+    }
+
+    // returns session id for this protocol run.
+    pub fn session_id(&self) -> &[u8] {
+        &self.aggregator.deal.session_id
+    }
+
+    // get_deal returns the Deal that this verifier has received. It returns
+    // error if the deal is not certified or there is not enough approvals.
+    pub fn get_deal(&self) -> Result<Deal, Box<dyn Error>> {
+        if !self.aggregator.enough_approvals() || !self.aggregator.deal_certified() {
+            bail!("Not enough approvals or deal not certified");
+        }
+
+        Ok(self.aggregator.deal.clone())
+    }
+
+    // process_response analyzes the given response. If it's a valid complaint, the
+    // verifier should expect to see a Justification from the Dealer. It returns an
+    // error if it's not a valid response.
+    // Call `v.deal_certified()` to check if the whole protocol is finished.
+    pub fn process_response(&mut self, resp: &Response) -> Result<(), Box<dyn Error>> {
+        self.aggregator.verify_response(resp)
+    }
+
+    // unsafe_set_response_dkg is an UNSAFE bypass method to allow DKG to use VSS
+    // that works on basis of approval only.
+    pub(crate) fn unsafe_set_response_dkg(
+        &mut self,
+        index: u32,
+        approved: bool,
+    ) -> Result<(), Box<dyn Error>> {
+        self.aggregator.unsafe_set_response_dkg(index, approved)
+    }
+
+    pub fn verifiers(&self) -> &[GE] {
+        &self.verifiers
+    }
+}
+
+impl Response {
+    pub fn hash(session_id: &[u8], index: u32, approved: u32) -> Result<[u8; 32], Box<dyn Error>> {
+        let mut hasher = Sha256::new();
+        hasher.write_all(b"response".as_ref()).unwrap();
+        hasher.write_all(session_id.as_ref()).unwrap();
+        hasher.write_all(&index.to_le_bytes()).unwrap();
+        hasher.write_all(&approved.to_le_bytes()).unwrap();
+        let result = hasher.result().as_slice().try_into()?;
+        Ok(result)
+    }
+
+    pub fn hash_self(&self) -> Result<[u8; 32], Box<dyn Error>> {
+        Response::hash(&self.session_id, self.index, self.approved as u32)
+    }
+}
+
+impl Justification {
+    pub fn hash(session_id: &[u8], index: u32, deal: &Deal) -> Result<[u8; 32], Box<dyn Error>> {
+        let mut hasher = Sha256::new();
+        hasher.write_all(b"justification".as_ref()).unwrap();
+        hasher.write_all(session_id.as_ref()).unwrap();
+        hasher.write_all(&index.to_le_bytes()).unwrap();
+        let deal_buff: Vec<u8> = bincode::serialize(deal)?;
+        hasher.write_all(deal_buff.as_ref()).unwrap();
+        let result = hasher.result().as_slice().try_into()?;
+        Ok(result)
+    }
+}
+
 impl Deal {
     /// analyzes the deal and returns an error if it's incorrect. If
     /// inclusion is true, it also returns an error if it the second time this struct
